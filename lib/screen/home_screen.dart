a178de58045bdace233bcd19bfd464b1ae9ac84c
heosangbeom <luv_3_u@naver.com>
2024-12-25 11:43:37 +0900

    video slide

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -20,6 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: Colors.black,
       body: video != null
           ? _VideoPlayer(
+              onAnotherVideoPicked: onLogoTab,
               video: video!,
             )
           : _VideoSelector(
@@ -123,8 +124,10 @@ class _VideoSelector extends StatelessWidget {
 
 class _VideoPlayer extends StatefulWidget {
   final XFile video;
+  final VoidCallback onAnotherVideoPicked;
 
   const _VideoPlayer({
+    required this.onAnotherVideoPicked,
     required this.video,
     super.key,
   });
@@ -134,12 +137,21 @@ class _VideoPlayer extends StatefulWidget {
 }
 
 class _VideoPlayerState extends State<_VideoPlayer> {
-  late final VideoPlayerController videoPlayerController;
+  late VideoPlayerController videoPlayerController;
+  bool showIcons = true;
 
   @override
   void initState() {
     super.initState();
     initializeController();
+  }
+
+  @override
+  void didUpdateWidget(covariant _VideoPlayer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.video.path != widget.video.path) {
+      initializeController();
+    }
   }
 
   initializeController() async {
@@ -159,118 +171,201 @@ class _VideoPlayerState extends State<_VideoPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AspectRatio(
-        aspectRatio: videoPlayerController.value.aspectRatio,
-        child: Stack(
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          showIcons = !showIcons;
+        });
+      },
+      child: Center(
+        child: AspectRatio(
+          aspectRatio: videoPlayerController.value.aspectRatio,
+          child: Stack(
+            children: [
+              VideoPlayer(
+                videoPlayerController,
+              ),
+              if (showIcons)
+                Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  color: Colors.black.withAlpha(128),
+                ),
+              if (showIcons)
+                _PlayButton(
+                  onReversePressed: onReversePressed,
+                  onPlayPressed: onPlayPressed,
+                  onForwardPressed: onForwardPressed,
+                  isPlaying: videoPlayerController.value.isPlaying,
+                ),
+              _Bottom(
+                position: videoPlayerController.value.position,
+                maxPosition: videoPlayerController.value.duration,
+                onSliderChanged: onSliderChanged,
+              ),
+              if (showIcons)
+                _PickAnotherVideo(
+                  onPressed: widget.onAnotherVideoPicked,
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onSliderChanged(double val) {
+    final position = Duration(seconds: val.toInt());
+
+    videoPlayerController.seekTo(position);
+  }
+
+  void onReversePressed() {
+    final currentPosition = videoPlayerController.value.position;
+
+    Duration position = Duration();
+    if (currentPosition.inSeconds > 3) {
+      position = currentPosition - Duration(seconds: 3);
+    }
+    videoPlayerController.seekTo(position);
+  }
+
+  void onPlayPressed() {
+    setState(() {
+      if (videoPlayerController.value.isPlaying) {
+        videoPlayerController.pause();
+      } else {
+        videoPlayerController.play();
+      }
+    });
+  }
+
+  void onForwardPressed() {
+    final maxPosition = videoPlayerController.value.duration;
+    final currentPosition = videoPlayerController.value.position;
+
+    Duration position = maxPosition;
+    if ((maxPosition - Duration(seconds: 3)).inSeconds >
+        currentPosition.inSeconds) {
+      position = currentPosition + Duration(seconds: 3);
+    }
+    videoPlayerController.seekTo(position);
+  }
+}
+
+class _PlayButton extends StatelessWidget {
+  final VoidCallback onReversePressed;
+  final VoidCallback onPlayPressed;
+  final VoidCallback onForwardPressed;
+  final bool isPlaying;
+
+  const _PlayButton({
+    required this.onReversePressed,
+    required this.onPlayPressed,
+    required this.onForwardPressed,
+    required this.isPlaying,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconButton(
+            color: Colors.white,
+            onPressed: onReversePressed,
+            icon: Icon(Icons.rotate_left),
+          ),
+          IconButton(
+            color: Colors.white,
+            onPressed: onPlayPressed,
+            icon: Icon(
+              isPlaying ? Icons.pause : Icons.play_arrow,
+            ),
+          ),
+          IconButton(
+            color: Colors.white,
+            onPressed: onForwardPressed,
+            icon: Icon(Icons.rotate_right),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Bottom extends StatelessWidget {
+  final Duration position;
+  final Duration maxPosition;
+  final ValueChanged<double> onSliderChanged;
+
+  const _Bottom({
+    required this.position,
+    required this.maxPosition,
+    required this.onSliderChanged,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 8.0,
+        ),
+        child: Row(
           children: [
-            VideoPlayer(
-              videoPlayerController,
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    color: Colors.white,
-                    onPressed: () {
-                      final currentPosition =
-                          videoPlayerController.value.position;
-
-                      Duration position = Duration();
-                      if (currentPosition.inSeconds > 3) {
-                        position = currentPosition - Duration(seconds: 3);
-                      }
-                      videoPlayerController.seekTo(position);
-                    },
-                    icon: Icon(Icons.rotate_left),
-                  ),
-                  IconButton(
-                    color: Colors.white,
-                    onPressed: () {
-                      setState(() {
-                        if (videoPlayerController.value.isPlaying) {
-                          videoPlayerController.pause();
-                        } else {
-                          videoPlayerController.play();
-                        }
-                      });
-                    },
-                    icon: Icon(
-                      videoPlayerController.value.isPlaying
-                          ? Icons.pause
-                          : Icons.play_arrow,
-                    ),
-                  ),
-                  IconButton(
-                    color: Colors.white,
-                    onPressed: () {
-                      final maxPosition = videoPlayerController.value.duration;
-                      final currentPosition =
-                          videoPlayerController.value.position;
-
-                      Duration position = maxPosition;
-                      if ((maxPosition - Duration(seconds: 3)).inSeconds >
-                          currentPosition.inSeconds) {
-                        position = currentPosition + Duration(seconds: 3);
-                      }
-                      videoPlayerController.seekTo(position);
-                    },
-                    icon: Icon(Icons.rotate_right),
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 8.0,
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      /// 0 -> 00
-                      /// 1 -> 01
-                      /// 11 -> 11
-                      '${videoPlayerController.value.position.inMinutes.toString()
-                          .padLeft(2,'0')}:${(videoPlayerController.value.position.inSeconds % 60).toString().padLeft(2,'0')}',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                    Expanded(
-                      child: Slider(
-                        value: videoPlayerController.value.position.inSeconds
-                            .toDouble(),
-                        max: videoPlayerController.value.duration.inSeconds
-                            .toDouble(),
-                        onChanged: (double val) {},
-                      ),
-                    ),
-                    Text(
-                      '${videoPlayerController.value.duration.inMinutes.toString().padLeft(2,'0')}:${(videoPlayerController.value.duration.inSeconds % 60).toString().padLeft(2,'0')}',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Positioned(
-              right: 0,
-              child: IconButton(
+            Text(
+              /// 0 -> 00
+              /// 1 -> 01
+              /// 11 -> 11
+              '${position.inMinutes.toString().padLeft(2, '0')}:${(position.inSeconds % 60).toString().padLeft(2, '0')}',
+              style: TextStyle(
                 color: Colors.white,
-                onPressed: () {},
-                icon: Icon(Icons.photo_camera_back),
+              ),
+            ),
+            Expanded(
+              child: Slider(
+                value: position.inSeconds.toDouble(),
+                max: maxPosition.inSeconds.toDouble(),
+                onChanged: onSliderChanged,
+              ),
+            ),
+            Text(
+              '${maxPosition.inMinutes.toString().padLeft(2, '0')}:${(maxPosition.inSeconds % 60).toString().padLeft(2, '0')}',
+              style: TextStyle(
+                color: Colors.white,
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _PickAnotherVideo extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const _PickAnotherVideo({
+    required this.onPressed,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 0,
+      child: IconButton(
+        color: Colors.white,
+        onPressed: onPressed,
+        icon: Icon(Icons.photo_camera_back),
       ),
     );
   }
